bb893f7da7c8d7d2eaacccb6751eb5ab6386ba2c
rhynox <86146107+rhynox-ui@users.noreply.github.com>
2026-08-26 11:16:35 +0100

    Update versionName to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 3
-        versionName = "1.0.2"
+        versionName = "1.0.4"
 
         vectorDrawables {
             useSupportLibrary = true
